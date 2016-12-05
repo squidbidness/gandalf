8339c2ec7aa7c82e0d9f6ba243ccf2428be2fce9
Anthony R. Hall <anthrond@gmail.com>
2016-12-04 22:37:40 -0700

    Add Vector {x,y,z,w}() getters

--- src/geo/vector.hpp
+++ src/geo/vector.hpp
@@ -10,10 +10,68 @@ namespace geo {
 	template< typename T, size_t N >
 	struct Vector : std::array<T, N> {
 
+		T &x() {
+			static_assert( N > 0 );
+			return (*this)[0];
+		}
+		T const &x() const {
+			return x();
+		}
+
+		T &y() {
+			static_assert( N > 1 );
+			return (*this)[1];
+		}
+		T const &y() const {
+			return y();
+		}
+
+		T &z() {
+			static_assert( N > 2 );
+			return (*this)[2];
+		}
+		T const &z() const {
+			return z();
+		}
+
+		T &w() {
+			static_assert( N > 3 );
+			return (*this)[3];
+		}
+		T const &w() const {
+			return w();
+		}
+
+		friend auto dot( Vector const &a, Vector const &b ) -> T {
+			T sum();
+			if constexpr ( N > 0 ) {
+				for ( size_t i = 0; i < a.size(); ++i ) {
+					sum += a[i] * b[i];
+				}
+			}
+			return sum;
+		}
+
 	};
 
-	using VectorF3 = Vector<float, 3>;
 
+	template< size_t N > using VectorF = Vector<float, N>;
+	template< size_t N > using VectorD = Vector<double, N>;
+
+	template< typename T > using Vector1 = Vector<T, 1>;
+	template< typename T > using Vector2 = Vector<T, 2>;
+	template< typename T > using Vector3 = Vector<T, 3>;
+	template< typename T > using Vector4 = Vector<T, 4>;
+
+	using VectorF1 = VectorF<1>;
+	using VectorF2 = VectorF<2>;
+	using VectorF3 = VectorF<3>;
+	using VectorF4 = VectorF<4>;
+
+	using VectorD1 = VectorD<1>;
+	using VectorD2 = VectorD<2>;
+	using VectorD3 = VectorD<3>;
+	using VectorD4 = VectorD<4>;
 }
 
 #endif
--- src/geo/vector_test_unit.cpp
+++ src/geo/vector_test_unit.cpp
@@ -4,12 +4,55 @@
 
 namespace geo {
 
-	TEST( Vector, stub ) {
-		VectorF3 v{ 0.0f, 1.0f, 2.0f };
+	template< typename ...S >
+	auto testComponents( S &&...s ) -> void {
+		Vector<std::common_type_t<S...>, sizeof...(S)> v
+				= { std::forward<S>( s )... };
 
-		EXPECT_EQ( 0.0f, v[0] );
-		EXPECT_EQ( 1.0f, v[1] );
-		EXPECT_EQ( 2.0f, v[2] );
+		if constexpr ( v.size() > 0 ) {
+			EXPECT_EQ( v[0], v.x() )
+					<< "v[0] = " << v[0] << ", v.x()=" << v.x();
+		}
+		if constexpr ( v.size() > 1 ) {
+			EXPECT_EQ( v[1], v.y() )
+					<< "v[1] = " << v[1] << ", v.y()=" << v.y();
+		}
+		if constexpr ( v.size() > 2 ) {
+			EXPECT_EQ( v[2], v.z() )
+					<< "v[2] = " << v[2] << ", v.z()=" << v.z();
+		}
+		if constexpr ( v.size() > 3 ) {
+			EXPECT_EQ( v[3], v.w() )
+					<< "v[3] = " << v[3] << ", v.w()=" << v.w();
+		}
+	}
+
+	TEST( Vector, component_getters ) {
+
+		testComponents( 0.0f );
+		testComponents( 0.0f, 1.0f );
+		testComponents( 0.0f, 1.0f, 2.0f );
+		testComponents( 0.0f, 1.0f, 2.0f, 3.0f );
+
+		testComponents( 0.0f );
+		testComponents( 0.0f, 1.0f );
+		testComponents( 0.0f, 1.0f, 2.0f );
+		testComponents( 0.0f, 1.0f, 2.0f, 3.0f );
+
+		testComponents( 0.0 );
+		testComponents( 0.0, 1.0 );
+		testComponents( 0.0, 1.0, 2.0 );
+		testComponents( 0.0, 1.0, 2.0, 3.0 );
+
+		testComponents( 0 );
+		testComponents( 0, 1 );
+		testComponents( 0, 1, 2 );
+		testComponents( 0, 1, 2, 3 );
+
+		testComponents( 0ul );
+		testComponents( 0ul, 1ul );
+		testComponents( 0ul, 1ul, 2ul );
+		testComponents( 0ul, 1ul, 2ul, 3ul );
 	}
 
 }
