24ceb9a96078f5a97cb39f12e9537ec571db1981
Anthony R. Hall <anthrond@gmail.com>
2017-02-23 19:48:28 -0700

    Move TestCase into its own new module, 'test_case'

--- tests/test_compile/test_case_builder.py
+++ tests/test_compile/test_case_builder.py
@@ -1,19 +1,5 @@
+from test_case import TestCase
 import parser
-
-class TestCase( object ):
-
-	def __init__( self, assertion, baseline_codes, codes ):
-		self.assertion = assertion
-		self.baseline_codes = baseline_codes
-		self.codes = codes
-
-	def __eq__( self, other ):
-		return (
-				self.assertion == other.assertion
-				and self.baseline_codes == other.baseline_codes
-				and self.codes == other.codes
-				)
-
 
 class TestCaseBuilder( object ):
 
