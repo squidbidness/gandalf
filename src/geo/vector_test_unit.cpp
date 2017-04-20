b1b6c899af92712b02a313a71c4854ca0d3a65ed
Anthony R. Hall <anthrond@gmail.com>
2017-04-19 21:41:16 -0600

    Add line numbers to Vector.component_getters test failures

--- src/geo/vector_test_unit.cpp
+++ src/geo/vector_test_unit.cpp
@@ -10,53 +10,57 @@ namespace geo {
 	using namespace std;
 
 	template< typename ...S >
-	auto testComponents( S &&...s ) -> void {
+	auto testComponents( size_t line, S &&...s ) -> void {
 		auto v = V_( std::forward<S>( s )... );
 
 		if constexpr ( v.size() > 0 ) {
 			EXPECT_EQ( v[0], v.x() )
-					<< "v[0] = " << v[0] << ", v.x()=" << v.x();
+					<< "LINE:" << line << " v[0] = " << v[0] << ", v.x()=" << v.x();
 		}
 		if constexpr ( v.size() > 1 ) {
 			EXPECT_EQ( v[1], v.y() )
-					<< "v[1] = " << v[1] << ", v.y()=" << v.y();
+					<< "LINE:" << line << " v[1] = " << v[1] << ", v.y()=" << v.y();
 		}
 		if constexpr ( v.size() > 2 ) {
 			EXPECT_EQ( v[2], v.z() )
-					<< "v[2] = " << v[2] << ", v.z()=" << v.z();
+					<< "LINE:" << line << " v[2] = " << v[2] << ", v.z()=" << v.z();
 		}
 		if constexpr ( v.size() > 3 ) {
 			EXPECT_EQ( v[3], v.w() )
-					<< "v[3] = " << v[3] << ", v.w()=" << v.w();
+					<< "LINE:" << line << " v[3] = " << v[3] << ", v.w()=" << v.w();
 		}
 	}
 
 	TEST( Vector, component_getters ) {
 
-		testComponents( 0.0f );
-		testComponents( 0.0f, 1.0f );
-		testComponents( 0.0f, 1.0f, 2.0f );
-		testComponents( 0.0f, 1.0f, 2.0f, 3.0f );
+#define TEST_COMPONENTS( ... ) testComponents( __LINE__, __VA_ARGS__ );
 
-		testComponents( 0.0f );
-		testComponents( 0.0f, 1.0f );
-		testComponents( 0.0f, 1.0f, 2.0f );
-		testComponents( 0.0f, 1.0f, 2.0f, 3.0f );
+		TEST_COMPONENTS( 0.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f, 2.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f, 2.0f, 3.0f );
 
-		testComponents( 0.0 );
-		testComponents( 0.0, 1.0 );
-		testComponents( 0.0, 1.0, 2.0 );
-		testComponents( 0.0, 1.0, 2.0, 3.0 );
+		TEST_COMPONENTS( 0.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f, 2.0f );
+		TEST_COMPONENTS( 0.0f, 1.0f, 2.0f, 3.0f );
 
-		testComponents( 0 );
-		testComponents( 0, 1 );
-		testComponents( 0, 1, 2 );
-		testComponents( 0, 1, 2, 3 );
+		TEST_COMPONENTS( 0.0 );
+		TEST_COMPONENTS( 0.0, 1.0 );
+		TEST_COMPONENTS( 0.0, 1.0, 2.0 );
+		TEST_COMPONENTS( 0.0, 1.0, 2.0, 3.0 );
 
-		testComponents( 0ul );
-		testComponents( 0ul, 1ul );
-		testComponents( 0ul, 1ul, 2ul );
-		testComponents( 0ul, 1ul, 2ul, 3ul );
+		TEST_COMPONENTS( 0 );
+		TEST_COMPONENTS( 0, 1 );
+		TEST_COMPONENTS( 0, 1, 2 );
+		TEST_COMPONENTS( 0, 1, 2, 3 );
+
+		TEST_COMPONENTS( 0ul );
+		TEST_COMPONENTS( 0ul, 1ul );
+		TEST_COMPONENTS( 0ul, 1ul, 2ul );
+		TEST_COMPONENTS( 0ul, 1ul, 2ul, 3ul );
+
+#undef TEST_COMPONENTS
 	}
 
 	TEST( Vector, dot ) {
